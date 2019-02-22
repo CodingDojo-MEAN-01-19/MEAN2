c664b2fbd7c7c6471b0e2405957068ee46aaa78c
dojo-json <jfranz@codingdojo.com>
2019-02-21 16:56:25 -0800

    components

--- books/src/app/app.component.ts
+++ books/src/app/app.component.ts
@@ -1,8 +1,4 @@
 import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
-
-import { Book } from './models';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,20 +6,4 @@ import { Book } from './models';
 })
 export class AppComponent {
   title = 'Angular';
-  book = new Book();
-  books: Book[] = [];
-
-  onSubmit(event: Event, form: NgForm) {
-    event.preventDefault();
-
-    console.log('submitting form', this.book);
-
-    this.books.push(this.book);
-
-    this.book = new Book();
-
-    console.log('books ', this.books);
-
-    form.reset();
-  }
 }
